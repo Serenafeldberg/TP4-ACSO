0f1f2d7105cf5423a4181930974a4a2bed2cd7c3
Serenafeldberg <sfeldberg@udesa.edu.ar>
2024-05-17 12:41:11 -0300

    main

--- src/main.cc
+++ src/main.cc
@@ -6,31 +6,31 @@
 
 using namespace std;
 
-void sleep_for(int slp){
-    this_thread::sleep_for(chrono::milliseconds(slp));
-}
+// void sleep_for(int slp){
+//     this_thread::sleep_for(chrono::milliseconds(slp));
+// }
 
-static mutex oslock;
+// static mutex oslock;
 
-static const size_t kNumThreads = 12;
-static const size_t kNumFunctions = 1000;
-static void simpleTest() {
-    ThreadPool pool(kNumThreads);
-    for (size_t id = 0; id < kNumFunctions; id++) {
-        pool.schedule([id] {
-            oslock.lock();
-            cout << "Thread (ID: " << id << ") has started." << endl;
-            oslock.unlock();
-            size_t sleepTime = (id % 3) * 10;
-            sleep_for(sleepTime);
-            oslock.lock();
-            cout << "Thread (ID: " << id << ") has finished." << endl ;
-            oslock.unlock();
-        });
-    }
+// static const size_t kNumThreads = 12;
+// static const size_t kNumFunctions = 1000;
+// static void simpleTest() {
+//     ThreadPool pool(kNumThreads);
+//     for (size_t id = 0; id < kNumFunctions; id++) {
+//         pool.schedule([id] {
+//             oslock.lock();
+//             cout << "Thread (ID: " << id << ") has started." << endl;
+//             oslock.unlock();
+//             size_t sleepTime = (id % 3) * 10;
+//             sleep_for(sleepTime);
+//             oslock.lock();
+//             cout << "Thread (ID: " << id << ") has finished." << endl ;
+//             oslock.unlock();
+//         });
+//     }
 
-    pool.wait();
-}
+//     pool.wait();
+// }
 
 // Function to compute the sum of a subvector
 void computeSum(const vector<int>& data, int start, int end, int* result) {
@@ -69,7 +69,7 @@ int main() {
     int totalSum = accumulate(results.begin(), results.end(), 0);
     cout << "Total sum of elements: " << totalSum << endl;
 
-    simpleTest();
+    //simpleTest();
 
     return 0;
 }
